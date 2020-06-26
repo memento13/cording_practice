718f483e30c4e890f84d349a1e850e4157a0fc2a
memento13 <61815697+memento13@users.noreply.github.com>
2020-06-26 22:01:38 +0900

    Update 9663.py

--- backpy/9663.py
+++ backpy/9663.py
@@ -1,4 +1,19 @@
 N = int(input())
-board = list()
-temprow = [0]*N
-print(temprow)
+count = 0
+col = [False]*N
+leftDia = [False]*(2*N-1)
+rightDia = [False]*(2*N-1)
+
+def QueenCK(row):
+    global count
+    if row == N:
+        count += 1
+        return
+    for i in range(N):
+        if not(col[i] or rightDia[row+i] or leftDia[row-i+N-1]):
+            col[i] = rightDia[row+i]=leftDia[row-i+N-1] = True
+            QueenCK(row+1)
+            col[i] = rightDia[row+i]=leftDia[row-i+N-1] = False
+
+QueenCK(0)
+print(count)
