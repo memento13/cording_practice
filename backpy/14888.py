44f769f80c0d74038ad875f5e12af581d1757b2b
memento13 <61815697+memento13@users.noreply.github.com>
2020-06-28 17:23:19 +0900

    Update 14888.py

--- backpy/14888.py
+++ backpy/14888.py
@@ -1,30 +1,32 @@
 N = int(input())
-global result
 global MAX,MIN
 N_list = list(map(int,input().split()))
-plus,minus,mul,div = map(int,input().splite())
+plus,minus,mul,div = map(int(input().split()))
 operator_list = list()
 operator_cheak = [True]*(N-1)
 operator_list.append([0]*plus)
 operator_list.append([1]*minus)
 operator_list.append([2]*mul)
 operator_list.append([3]*div)
-result = N_list[0]
 
-def calculator(i,num):
+def calculator(i,num,result):
     if i==0:
         result = result +num
+        return result
     if i==1:
         result = result -num
+        return result
     if i==2:
         result = result *num
+        return result
     if i==3:
-        if Num<0:
-            result = -1*(result//abs(Num))
+        if num<0:
+            result = -1*(result//abs(num))
         else:
-            result = result // Num
+            result = result // num
+        return result
         
-def cal(i):
+def cal(i,result):
     if i==N-1:
         if result>MAX or MAX == None:
             MAX = result
@@ -34,5 +36,10 @@ def cal(i):
     
     for i in range(N-1):
         if operator_cheak[i]:
-            
+            operator_cheak[i] = False
+            result = calculator(operator_list[i],N_list[i+1],result)
+            operator_cheak[i] = True
 
+cal(0,N_list[0])
+print(MAX)
+print(MIN)
