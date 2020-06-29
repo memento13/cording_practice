fdc9db93045f0d167b8baba5eff0dcfe61e49504
memento13 <61815697+memento13@users.noreply.github.com>
2020-06-29 13:11:10 +0900

    Update 14888.py

--- backpy/14888.py
+++ backpy/14888.py
@@ -1,13 +1,13 @@
 N = int(input())
 global MAX,MIN
 N_list = list(map(int,input().split()))
-plus,minus,mul,div = map(int(input().split()))
-operator_list = list()
-operator_cheak = [True]*(N-1)
-operator_list.append([0]*plus)
-operator_list.append([1]*minus)
-operator_list.append([2]*mul)
-operator_list.append([3]*div)
+operator_list = list(map(int,input().split()))
+
+def make_operator_dict(operator_list):
+    operator_dict = dict()
+    for i,c in enumerate(operator_list):
+        operator_dict[i] = c
+    return operator_dict
 
 def calculator(i,num,result):
     if i==0:
@@ -21,25 +21,19 @@ def calculator(i,num,result):
         return result
     if i==3:
         if num<0:
-            result = -1*(result//abs(num))
+            result = -1*(-result//abs(num))
         else:
             result = result // num
         return result
-        
-def cal(i,result):
-    if i==N-1:
-        if result>MAX or MAX == None:
-            MAX = result
-        if result<MIN or MIN == None:
-            MIN = result
-        return
-    
-    for i in range(N-1):
-        if operator_cheak[i]:
-            operator_cheak[i] = False
-            result = calculator(operator_list[i],N_list[i+1],result)
-            operator_cheak[i] = True
 
-cal(0,N_list[0])
-print(MAX)
-print(MIN)
+operator_dict = make_operator_dict(operator_dict)
+result_list = list()
+        
+def cal(N_list, operator_dict):
+    print(N_list,operator_dict)
+    if len(N_list)==1:
+        result_list.append(N_list[0])
+        return 0
+    else:
+        result = N_list.pop(0)
+        
