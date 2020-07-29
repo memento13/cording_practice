73769ea17d1cdf11f0bbfc02ea4ed4512bef7119
memento13 <61815697+memento13@users.noreply.github.com>
2020-07-29 18:58:39 +0900

    Update 1987.py

--- backpy/1987.py
+++ backpy/1987.py
@@ -2,30 +2,29 @@ from sys import stdin as st
 
 R,C = map(int,st.readline().split())
 board = []
-MAX = 0
-visited = [0]*26
+MAX = 1
+visited = []
 for i in range(R):
     board.append(list(st.readline().strip()))
 
 def DFS(i,j,count):
     global MAX
-    if i>R-1 or i<0:
-        return
-    if j>C-1 or j<0:
-        return
-    else:
-        temp = ord(board[i][j])-ord("A")
-        if visited[temp]==0:
-            visited[temp] = 1
-            count+=1
-            if count>MAX:
-                MAX = count
+    if count>MAX:
+        MAX = count
+    if board[i][j] not in visited:
+        count+=1
+        visited.append(board[i][j])
+        if (0<=(i+1)<R) and (0<=(j)<C):
             DFS(i+1,j,count)
+        if (0<=(i-1)<R) and (0<=(j)<C):
             DFS(i-1,j,count)
+        if (0<=(i)<R) and (0<=(j+1)<C):
             DFS(i,j+1,count)
+        if (0<=(i)<R) and (0<=(j-1)<C):
             DFS(i,j-1,count)
-        else:
-            return
-            
+        visited.remove(board[i][j])
+    else:
+        return
+
 DFS(0,0,0)
 print(MAX)
