8b51d67bb0134c466c6bfc363ac1c51a178fa78f
memento13 <61815697+memento13@users.noreply.github.com>
2020-08-23 22:06:32 +0900

    Update 1932.py

--- backpy/1932.py
+++ backpy/1932.py
@@ -6,6 +6,9 @@ for i in range(N):
     tree.append(list(map(int,sys.stdin.readline().split())))
 
 for i in range(N-1):
-    for j in range(len(tree[i])):
-        tree[i+1][j] = 
-print(tree)
+    for j in range(len(tree[i+1])):
+        tree[i+1][j] += tree[i][j//2]
+temp = []
+for i in tree[N-1]:
+    temp.append(i)
+print(max(temp))
