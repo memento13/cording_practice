81fed7d2c5e26981e72c163c5df36922c63b9eb2
memento13 <61815697+memento13@users.noreply.github.com>
2020-08-27 22:32:39 +0900

    Update 2579.py

--- backpy/2579.py
+++ backpy/2579.py
@@ -6,24 +6,12 @@ for _ in range(N):
     step.append(int(sys.stdin.readline()))
 
 step.reverse()
-streak = 0
-i=0
-while(i<N-1):
-    if streak<2:
-        if i==N-2:
-            step[i+1]+=step[i]
-            i+=1
-        else:
-            if step[i+1]>step[i+2]:
-                step[i+1]+=step[i]
-                streak+=1
-                i+=1
-            else:
-                step[i+2]+=step[i]
-                streak=0
-                i+=2
-    else:
-        streak=0
-        step[i+1]+=step[i]
-        i+=1
-print(step[-1])
+result = [0 for _ in range(N)]
+
+result[0]=step[0]
+result[1]=step[0]+step[1]
+result[2] = max(step[1]+step[2],step[0]+step[2])
+
+for i in range(3,N):
+    result.append(max(step[i]+step[i-1]+result[i-3],step[i]+result[i-2]))
+print(result[-1])
