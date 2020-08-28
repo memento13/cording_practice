bf749ea89e7b77c5714f49a28f410bdfab54c615
memento13 <61815697+memento13@users.noreply.github.com>
2020-08-28 23:01:28 +0900

    Create 2601.py

---
+++ codeup/2601.py
@@ -0,0 +1,13 @@
+import sys
+
+fivlist = [1,1,2,3,5,8]
+def fiv(i):
+    if i<=len(fivlist):
+        return fivlist[i-1]
+    else:
+        temp = fiv(i-1)+fiv(i-2)
+        fivlist.append(temp)
+        return temp
+
+N = int(sys.stdin.readline())
+print(fiv(N))
