f3473922e16f5b568d11b73e4b1f1b5d7aa5200d
memento13 <61815697+memento13@users.noreply.github.com>
2020-08-31 21:04:24 +0900

    Update 2579.py

--- backpy/2579.py
+++ backpy/2579.py
@@ -5,13 +5,14 @@ step = []
 for _ in range(N):
     step.append(int(sys.stdin.readline()))
 
-step.reverse()
 result = [0 for _ in range(N)]
+if N<3:
+    print(sum(step))
+else:
+    result[0]=step[0]
+    result[1]=step[0]+step[1]
+    result[2] = max(step[1]+step[2],step[0]+step[2])
 
-result[0]=step[0]
-result[1]=step[0]+step[1]
-result[2] = max(step[1]+step[2],step[0]+step[2])
-
-for i in range(3,N):
-    result.append(max(step[i]+step[i-1]+result[i-3],step[i]+result[i-2]))
-print(result[-1])
+    for i in range(3,N):
+        result[i] = (max(step[i]+step[i-1]+result[i-3],step[i]+result[i-2]))
+    print(result[-1])
