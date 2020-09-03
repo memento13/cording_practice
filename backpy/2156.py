35751011ad726c5ffd0b761d97166e7fa16e799a
memento13 <61815697+memento13@users.noreply.github.com>
2020-09-03 22:28:36 +0900

    Update 2156.py

--- backpy/2156.py
+++ backpy/2156.py
@@ -11,8 +11,8 @@ if N<3:
 else:
     result[0]=step[0]
     result[1]=step[0]+step[1]
-    result[2] = max(step[1]+step[2],step[0]+step[2])
+    result[2] = max(step[1]+step[2],step[0]+step[2],result[1])
 
     for i in range(3,N):
-        result[i] = (max(step[i]+step[i-1]+result[i-3],step[i]+result[i-2]))
-    print(max(result))
+        result[i] = (max(result[i-1],result[i-3]+step[i-1]+step[i],result[i-2]+step[i]))
+    print(result[-1])
