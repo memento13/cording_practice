33bcb005d37e31882aa193fc5466b03b9207d91f
memento13 <61815697+memento13@users.noreply.github.com>
2020-09-17 22:04:39 +0900

    2020.9.17

--- backpy/temp.py
+++ backpy/temp.py
@@ -28,9 +28,18 @@ for i in range(n):
             sheep+= s; wolf+= w
 print(sheep, wolf)
 """
-n = int(input())
+a = [242, 256, 237, 223, 263, 81, 46]
+print('A = ', a)
 
-for i in range(1, n+1):
-    if i%2 == 0:
-        print(' ', end="")
-    print("* " * n)
+import matplotlib.pyplot as plt
+from matplotlib import font_manager, rc
+font_name = font_manager.FontProperties(fname='c:/Windows/Fonts/malgun.ttf').get_name()
+rc('font',family = font_name)
+
+x_data = ['MON', 'TUE', 'WED', 'THR', 'FRI', 'SAT', 'SUN']
+plt.title('일주일간 유동 인구수 데이터', fontsize = 16)
+plt.xlabel('요일', fontsize = 12)
+plt.ylabel('유동 인구수', fontsize = 12)
+plt.scatter(x_data, a)
+plt.plot(x_data, a)
+plt.show( )
